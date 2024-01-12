6378e244e82844c46010caa0ab55a634c5ec421a
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2024-01-12 21:40:00 +0100

    get sentence

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-console.log('hello world!');
-const base_url = 'https://flynn.boolean.careers/exercises/api/random/word'
+const baseUrl = 'https://flynn.boolean.careers/exercises/api/random/word';
+const maxWord = 10;
+const result = document.getElementById('randomWords')
+let words = [];
 
+function fetchWord() {
+    axios.get(baseUrl)
+        .then(response => {
+            let word = response.data.response
+            //console.log(word);
+            words.push(word);
 
-axios.get(base_url)
-    .then(response => {
-        console.log(response.data.response);
+            if (words.length == maxWord) {
+                //console.log(words);
+                const sentence = words.join(' ')
+                console.log('sentence = ' + sentence);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
 
-    })
-    .catch(error => {
-        console.log(error);
-    });
+for (let i = 0; i < maxWord; i++) {
+    fetchWord();
+}
