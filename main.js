f063627f504907d70af8e5d42a1fb86a4ccc9bff
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2024-01-12 21:04:43 +0100

    add cdn axios + get 1st word

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('hello world!');
+const base_url = 'https://flynn.boolean.careers/exercises/api/random/word'
+
+
+axios.get(base_url)
+    .then(response => {
+        console.log(response.data.response);
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
